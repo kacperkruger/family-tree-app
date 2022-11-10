9e6374b5a4511db8419c367299a3a4f863a19123
kacperkruger <kacper.kruger1@gmail.com>
2022-11-10 15:03:06 +0100

    added api implementation

--- lab_05/routes/users.js
+++ lab_05/routes/users.js
@@ -5,43 +5,83 @@ const User = require('../models/User');
 
 // Pobranie danych wszystkich użytkowników
 router.get('/', async (req, res) => {
-    return res.send({
-        allUsers: []
-    });
+    const users = await User.find({})
+    return res.send({allUsers: users})
 });
 
 // Utworzenie nowego użytkownika
 router.post('/', async (req, res) => {
-    return res.send(req.body);
+    const data = req.data
+
+    try {
+        const newUser = await User.create({ 
+            login: data.login,
+            email: data.email,
+            registrationDate: data.registrationDate
+        })
+        return res.status(201).send({newUser: user})
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send(e.getMessage())
+    }
 });
 
 // Pobranie danych użytkownika o podanym userId
 router.get('/:userId', async (req, res) => {
-    return res.send({});
+    const id = request.params.userId
+    try {
+        user = User.findOne({_id: id})
+        return res.send(user)
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send(e)
+    }
 });
 
 // Zastąpienie danych użytkownika o podanym userId nowym „kompletem”
 router.put('/:userId', async (req, res) => {
     const id = req.params.userId;
-    return res.send({
-        putUserId: id
-    });
+    const data = req.data;
+    
+    try {
+        const updatedUser = await User.findOneAndUpdate({_id: id}, {
+            login: data.login,
+            email: data.email,
+            registrationDate: data.registrationDate
+        })
+        return res.send(updatedUser)
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send(e);
+    }
 });
 
 // Usuniecie użytkownika o podanym userId
 router.delete('/:userId', async (req, res) => {
     const id = req.params.userId;
-    return res.send({
-        deletedUserId: id
-    });
+    
+    try {
+        const deletedUser = await User.findOneAndDelete({_id: id});
+        return res.send({deletedUser})
+    }
 });
 
 // „Unacześnienie” wybranych danych użytkownika o podanym userId
 router.patch('/:userId', async (req, res) => {
     const id = req.params.userId;
-    return res.send({
-        patchUserId: id
-    });
+    
+    try {
+        const user = await User.findOne({_id: id})
+        const updatedUser = await User.findOneAndUpdate({_id: id}, {
+            login: data.login || user.login,
+            email: data.email || user.email,
+            registrationDate: data.registrationDate || user.registrationDate
+        })
+        return res.send(updatedUser)
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send(e);
+    }
 });
 
 module.exports = router;
