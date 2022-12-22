975509867b3a60f01560dc01a7adf63973cbd499
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-22 15:31:16 +0100

    kod z zajec

---
+++ lab_12/src/components/DetailsInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li>
+      <strong>telefon:</strong>
+      {{ phoneNumber }}
+    </li>
+    <li>
+      <strong>e-mail:</strong>
+      {{ emailAddress }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "DetailsInfo",
+  inject: ['phoneNumber', 'emailAddress'],
+  data() {
+    return {
+      phone: this.phoneNumber,
+      emailAddress: this.emailAddress
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
