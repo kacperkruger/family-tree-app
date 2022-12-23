c9f0ee44247389b3a54e17a1d677c9c9749a8b00
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-23 01:14:13 +0100

    modified to return error message

--- private-chat-service/utils/parseError.ts
+++
@@ -1,9 +0,0 @@
-const parseError = (e: unknown, f: Function) => {
-    if (typeof e === 'string') {
-        f(e.toUpperCase());
-    } else if (e instanceof Error) {
-        f(e.message);
-    }
-};
-
-export default parseError;
