447d32dec298d83bf8e284c8eee05a20d31e8ea2
Kacper Kruger <kacper.kruger1@gmail.com>
2022-12-23 01:16:00 +0100

    configired api routes

--- private-chat-service/index.ts
+++ private-chat-service/index.ts
@@ -1,17 +1,22 @@
 import connectToMongo from './utils/connectToMongo';
 import express from 'express';
 import startServer from './utils/startServer';
-import parseError from './utils/parseError';
+import parseErrorMessage from './utils/parseErrorMessage';
 import dotenvConfig from './configs/dotenvConfig';
 import expressConfig from './configs/expressConfig';
+import apiConfig from './configs/apiConfig';
+import mongooseConfig from './configs/mongooseConfig';
 
 const app = express();
 
 dotenvConfig();
 expressConfig(app);
+apiConfig(app);
 
 connectToMongo().then(_ => {
+    mongooseConfig();
     startServer(app);
 }).catch(e => {
-    parseError(e, console.log);
+    const errorMessage = parseErrorMessage(e);
+    console.log(errorMessage);
 });
