d916f95331165259a11f94e55f0203c2eab43f84
Kacper Kruger <kacper.kruger1@gmail.com>
2023-01-03 12:32:02 +0100

    added query for adding parent relationship

---
+++ family-tree-service/queries/addParentRelationship.ts
@@ -0,0 +1,16 @@
+const addParentRelationship = 'MERGE (user: User {userId: $userId})\n' +
+    'WITH user\n' +
+    'MATCH (user)-[:OWNS]->(child: Person {id: $childId})\n' +
+    'OPTIONAL MATCH (child)-[:CHILD_OF]->(parents: Person)\n' +
+    'WITH user, child, count(parents) AS numberOfParents\n' +
+    'WHERE numberOfParents < 2\n' +
+    'WITH user, child\n' +
+    'MATCH (user)-[:OWNS]->(newParent: Person {id: $parentId})\n' +
+    'MERGE (child)-[:CHILD_OF]->(newParent)\n' +
+    'WITH child, newParent\n' +
+    'UNWIND [child, newParent] AS editedPerson\n' +
+    'OPTIONAL MATCH (editedPerson)-[:CHILD_OF]->(parents: Person)\n' +
+    'OPTIONAL MATCH (editedPerson)-[:PARTNERS]-(partners: Person)\n' +
+    'RETURN editedPerson.id AS id, editedPerson.name AS name, collect(parents.id) AS parents, collect(partners.id) AS partners';
+
+export default addParentRelationship;
