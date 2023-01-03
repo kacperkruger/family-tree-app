32253b14000722bb173ad56e65bfb3e1c74f2807
Kacper Kruger <kacper.kruger1@gmail.com>
2023-01-04 00:21:34 +0100

    added api config

--- family-tree-service/index.ts
+++ family-tree-service/index.ts
@@ -4,11 +4,13 @@ import parseErrorMessage from './utils/parseErrorMessage';
 import dotenvConfig from './configs/dotenvConfig';
 import expressConfig from './configs/expressConfig';
 import connectToNeo4j from './utils/connectToNeo4j';
+import apiConfig from './configs/apiConfig';
 
 const app = express();
 
 dotenvConfig();
 expressConfig(app);
+apiConfig(app);
 
 connectToNeo4j().then(_ => {
     startServer(app);
