7ddc7ce7674864481c2b3f72b6d57d755ddadb20
Kacper Kruger <kacper.kruger1@gmail.com>
2023-01-04 00:21:14 +0100

    added person parser from record

--- family-tree-service/utils/connectToNeo4j.ts
+++ family-tree-service/utils/connectToNeo4j.ts
@@ -1,9 +1,19 @@
-import neo4j from 'neo4j-driver';
+import neo4j, {Driver} from 'neo4j-driver';
 import neo4jConnData from '../configs/neo4jConnData';
 
-const connectToNeo4j = async () => {
-    const driver = neo4j.driver(neo4jConnData.uri, neo4j.auth.basic(neo4jConnData.user, neo4jConnData.password));
-    console.log(await driver.getServerInfo());
-};
+const connectToNeo4j = (() => {
+    let driver: Driver;
+    let executed = false;
+
+    return async () => {
+        if (!executed) {
+            executed = true;
+            const dbConnData = neo4jConnData();
+            driver = neo4j.driver(dbConnData.uri, neo4j.auth.basic(dbConnData.user, dbConnData.password));
+            console.log('Connected to Neo4j');
+        }
+        return driver.session();
+    };
+})();
 
 export default connectToNeo4j;
---
+++ family-tree-service/utils/parsePerson.ts
@@ -0,0 +1,16 @@
+import {Person} from '../models/Person';
+import {Record} from 'neo4j-driver';
+
+const parsePerson = (record: Record): Person => {
+    return {
+        id: record.get('id'),
+        name: record.get('name'),
+        surname: record.get('surname'),
+        gender: record.get('gender'),
+        dateOfBirth: new Date(record.get('dateOfBirth')).toISOString().split('T')[0],
+        parents: record.get('parents'),
+        partners: record.get('partners')
+    };
+};
+
+export default parsePerson;
