5297ce32d908157ddc37b125d402c70fbbd82f34
Kacper Kruger <kacper.kruger1@gmail.com>
2023-01-06 23:40:02 +0100

    added get person functon

---
+++ project/family-tree-service/operations/getPerson.ts
@@ -0,0 +1,16 @@
+import connectToNeo4j from '../utils/connectToNeo4j';
+import findPersonById from '../queries/findPersonById';
+import parsePerson from '../utils/parsePerson';
+import {Person} from '../models/Person';
+
+const getPerson = async (personId: string): Promise<Person> => {
+    const session = await connectToNeo4j();
+    const result = session.run(findPersonById, {personId});
+
+    return result.then(result => {
+        if (!result.records.length) throw new Error('Person does not exists');
+        return result.records.map(record => parsePerson(record))[0];
+    }).finally(() => session.close);
+};
+
+export default getPerson;
