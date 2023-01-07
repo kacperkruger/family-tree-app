6acb8e61b6a42c9f7b5ef1a2da6446a237e566c2
Kacper Kruger <kacper.kruger1@gmail.com>
2023-01-08 00:00:31 +0100

    changed return type to return list of person

--- project/family-tree-service/operations/addPartnerRelationship.ts
+++ project/family-tree-service/operations/addPartnerRelationship.ts
@@ -3,7 +3,7 @@ import {Person} from '../models/Person';
 import parsePerson from '../utils/parsePerson';
 import addPartner from '../queries/addPartner';
 
-const addPartnerRelationship = async (partner1Id: string, partner2Id: string): Promise<Person> => {
+const addPartnerRelationship = async (partner1Id: string, partner2Id: string): Promise<Person[]> => {
     const session = await connectToNeo4j();
 
     const result = session.run(addPartner, {
@@ -14,7 +14,7 @@ const addPartnerRelationship = async (partner1Id: string, partner2Id: string): P
     return result.then(resultQuery => {
         const records = resultQuery.records;
         if (!records.length) throw new Error('User(s) not found');
-        return records.map(record => parsePerson(record))[0];
+        return records.map(record => parsePerson(record));
     }).finally(() => session.close());
 };
 
