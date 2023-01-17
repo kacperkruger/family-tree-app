d57bd134b559f369e7cd61f1d8bcabc7cc018830
Kacper Kruger <kacper.kruger1@gmail.com>
2023-01-17 23:35:45 +0100

    exported family tree client

---
+++ project/clients/src/family-tree/index.ts
@@ -0,0 +1,23 @@
+import addPartnership from './addPartnership';
+import addParentRelationship from './addParentRelationship';
+import editPerson from './editPerson';
+import getFamilyTree from './getFamilyTree';
+import getUsersBySurnames from './getUsersBySurnames';
+import copyPerson from './copyPerson';
+import deletePerson from './deletePerson';
+import deletePartnership from './deletePartnership';
+import deleteParentRelationship from './deleteParentRelationship';
+import addPerson from './addPerson';
+
+export {
+    addPartnership,
+    addParentRelationship,
+    editPerson,
+    getFamilyTree,
+    getUsersBySurnames,
+    copyPerson,
+    deletePerson,
+    deletePartnership,
+    deleteParentRelationship,
+    addPerson
+};
