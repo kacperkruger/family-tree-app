9378f5e9dd3ff23af81358e2fbce5cd1a91241fd
Kacper Kruger <kacper.kruger1@gmail.com>
2023-01-28 21:45:26 +0100

    added search user by surname

--- project/web-client/src/stores/users.ts
+++ project/web-client/src/stores/users.ts
@@ -51,5 +51,25 @@ export const useUsersStore = defineStore('users', () => {
         }
     }
 
-    return { users, getAllUsers, getUser, getUserByUsername }
+    const getUsersByPersonsDetails = async (surnames: string[], dateOfBirth?: string) => {
+        if (!surnames.length) {
+            await getAllUsers()
+            return
+        }
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_API_HOST_URL}/api/v1/family-trees/users?${surnames.map(surname => `surname=${surname}`).join('&')}${dateOfBirth ? `&dateOfBirth=${dateOfBirth}` : ''}`, {withCredentials: true})
+            users.value = response.data.users
+        } catch (e) {
+            if (isAxiosError(e) && e.response?.status === 401) {
+                authStore.logout()
+                await router.push({name: 'login'})
+            }
+            if (isAxiosError(e) && e.response?.status === 404) {
+                users.value = []
+            }
+            console.log(e)
+        }
+    }
+
+    return { users, getAllUsers, getUser, getUserByUsername, getUsersByPersonsSurnames: getUsersByPersonsDetails }
 })
--- project/web-client/src/views/UserListView.vue
+++ project/web-client/src/views/UserListView.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import {useUsersStore} from "@/stores/users";
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import {useRouter} from "vue-router";
 
 const usersStore = useUsersStore();
 const router = useRouter();
+
+const searchSurnames = ref('')
+const searchDateOfBirth = ref<string | undefined>(undefined)
+
+const searchUsersByPersonSurname = async () => {
+  await usersStore.getUsersByPersonsSurnames(searchSurnames.value.split(' '), searchDateOfBirth.value)
+}
 
 onMounted(async () => {
   await usersStore.getAllUsers();
@@ -14,9 +21,18 @@ onMounted(async () => {
 <template>
   <div class="view flex-col w-full overflow-auto gap-4 p-4">
     <div class="flex gap-2 w-full items-center justify-between">
-
       <h1 class="text-4xl">User List</h1>
-      <input type="text" class="border p-2 rounded self-center" placeholder="Search in users...">
+      <div class="flex flex-col border rounded">
+        <div class="flex items-center border-b-2">
+          <input v-model.trim="searchSurnames" type="text" class="p-2" placeholder="Search in users...">
+          <button class="p-2" @click="searchUsersByPersonSurname">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:stroke-2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+          </button>
+        </div>
+        <input v-model="searchDateOfBirth" class="p-2 bg-gray-100 cursor-pointer" type="date">
+      </div>
     </div>
     <div class="text-lg flex flex-grow justify-center overflow-auto w-full">
       <div class="flex flex-col w-max gap-1">
