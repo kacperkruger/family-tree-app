d257ea8c11919ed3e28f3d213d5303694408a170
Kacper Kruger <kacper.kruger1@gmail.com>
2023-02-01 16:53:40 +0100

    added find user by username

--- project/api-gateway/src/routes/users.ts
+++ project/api-gateway/src/routes/users.ts
@@ -1,14 +1,37 @@
 import express, {Request, Response} from 'express';
-import {getUsersDetails} from '@kacperkruger/clients/user';
+import {getUserDetailsByUsername, getUsersDetails} from '@kacperkruger/clients/user';
 import {isClientError} from '@kacperkruger/clients';
 import {parseErrorMessage} from '@kacperkruger/common-server-utils';
 
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
+    const userId = <string>req.user?._id;
     try {
         const users = await getUsersDetails([]);
-        res.json({users});
+        res.json({users: users.filter(user => user._id !== userId)});
+    } catch (e) {
+        if (isClientError(e)) return res.status(e.response?.status || 500).json({error: e.response?.data.error});
+        return res.status(500).json({error: parseErrorMessage(e)});
+    }
+});
+
+router.get('/:userId', async (req: Request, res: Response) => {
+    const userId = req.params.userId;
+    try {
+        const user = await getUsersDetails([userId]);
+        res.json({user: user[0]});
+    } catch (e) {
+        if (isClientError(e)) return res.status(e.response?.status || 500).json({error: e.response?.data.error});
+        return res.status(500).json({error: parseErrorMessage(e)});
+    }
+});
+
+router.get('/usernames/:username', async (req: Request, res: Response) => {
+    const username = req.params.username;
+    try {
+        const user = await getUserDetailsByUsername(username);
+        res.json({user: user});
     } catch (e) {
         if (isClientError(e)) return res.status(e.response?.status || 500).json({error: e.response?.data.error});
         return res.status(500).json({error: parseErrorMessage(e)});
