ae823e3b58706933b3a874491f57ce78bc156aab
Kacper Kruger <kacper.kruger1@gmail.com>
2023-02-10 00:55:41 +0100

    added emmiting creating private chat

--- project/websocket-service/src/index.ts
+++ project/websocket-service/src/index.ts
@@ -1,31 +1,46 @@
-import express from 'express';
 import http from 'http';
-import {Server} from 'socket.io';
-import cors from 'cors';
+import {Server, Socket} from 'socket.io';
 import dotenv from 'dotenv';
+import {PrivateChatResponse} from '@kacperkruger/clients/dist/private-chat/domain/PrivateChatResponse';
 
-const app = express();
-app.use(cors());
 dotenv.config();
 
-const server = http.createServer(app);
+const server = http.createServer();
 const io = new Server(server, {
     cors: {
-        origin: '*'
+        origin: [process.env.WEB_CLIENT_HOST_URL || '']
     }
 });
 
-const workspaces = io.of(/^\/\w+$/);
+const workspaces = io.of(/^\/chat\/private\/\w+\/messages$/);
 
 workspaces.on('connection', async socket => {
-    const workspace = socket.nsp;
-    const name = workspace.name;
-    console.log(name);
     socket.on('message', data => {
         socket.broadcast.emit('message', data);
     });
 });
 
+io.of('/chat/public').on('connection', async socket => {
+    socket.on('message', data => {
+        socket.broadcast.emit('message', data);
+    });
+});
+
+const userIdSocketMap = new Map<string, Socket>();
+io.of('/chat/private').on('connection', async socket => {
+    const userId = socket.handshake.auth.userId;
+    userIdSocketMap.set(userId, socket);
+
+    socket.on('message', (data: PrivateChatResponse) => {
+        data.users.forEach(user => {
+            const userSocket = userIdSocketMap.get(user._id);
+            if (!userSocket || userSocket === socket) return;
+            socket.to(userSocket.id).emit('message', data);
+        });
+    });
+});
+
 const port = process.env.PORT;
-server.listen(port);
-console.log(`API server listening at http://localhost:${port}`);
+server.listen(port, () => {
+    console.log(`API server listening at http://localhost:${port}`);
+});
